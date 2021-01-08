6e571b836c2748e4316f0d8a94e8cf9290f84a2c
fiixed <fiixed@gmail.com>
2021-01-08 10:12:02 -0500

    finish first and second

--- comments/app.js
+++ comments/app.js
@@ -1,17 +1,17 @@
-let ProfilePic = () => {
-    return <span>Make this component render an image</span>
-}
-
-let CommentBody = () => {
-    return <span>Make this component render a username in bold above the comment text</span>
-}
-
-let Comment = () => {
-    return <span>Make each comment render a profile picture and the comment body</span>
-}
+import Comment from '../comments-full/src/components/Comment';
+import comments from '../comments-full/src/comments';
 
 let Comments = () => {
-    return <span>Make this component render a list of comments</span>
+    return <div>
+    {
+        comments.map((comment) => (
+            <Comment
+                pic={comment.pic}
+                body={comment.body}
+                />
+        ))};
+
+    </div>
 }
 
 ReactDOM.render(<Comments />, document.getElementById('root'))
